fd391bf2daca422d4574725d31f95c9dd80bb3cb
Alexandra Freeman <alexandra@hashicorp.com>
2021-03-05 08:30:50 -0500

    Adding Nori to balance the Franks

--- image.tf
+++ image.tf
@@ -14,3 +14,21 @@ resource "google_storage_object_access_control" "frank_image_public" {
 output "frank_url" {
   value = google_storage_bucket_object.frank_image.self_link
 }
+
+
+resource "google_storage_bucket_object" "nori_image" {
+  name   = "nori.jpg"
+  source = "assets/nori.jpg"
+  bucket = google_storage_bucket.bucket.name
+}
+
+resource "google_storage_object_access_control" "nori_image_public" {
+  object = google_storage_bucket_object.nori_image.output_name
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+output "nori_url" {
+  value = google_storage_bucket_object.nori_image.self_link
+}
