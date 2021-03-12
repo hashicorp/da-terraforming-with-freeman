8f7233c94e3a44c637365615baffba7eb3c5090b
Alexandra Freeman <alexandra@hashicorp.com>
2021-03-12 09:26:59 -0500

    Allowing unauthenticated requests.

--- thumbnail.tf
+++ thumbnail.tf
@@ -57,3 +57,20 @@ resource "google_cloud_run_service" "resize" {
     latest_revision = true
   }
 }
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.resize.location
+  project     = google_cloud_run_service.resize.project
+  service     = google_cloud_run_service.resize.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
