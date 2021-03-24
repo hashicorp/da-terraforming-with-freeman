542afb8877c5561dc19bd167a638b6700a4360f1
Alexandra Freeman <alexandra@hashicorp.com>
2021-03-24 10:05:40 -0400

    added cloud build trigger.

--- main.tf
+++ main.tf
@@ -19,3 +19,6 @@ terraform {
 provider "google" {
   project = "terraforming-with-freeman"
 }
+
+data "google_project" "project" {
+}
--- repo.tf
+++ repo.tf
@@ -5,3 +5,23 @@ resource "google_sourcerepo_repository" "website_repo" {
 output "website_repo_url" {
   value = google_sourcerepo_repository.website_repo.url
 }
+
+resource "google_cloudbuild_trigger" "build" {
+  trigger_template {
+    branch_name = "master"
+    repo_name   = google_sourcerepo_repository.website_repo.name
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = [ "build", "-t", "gcr.io/${data.google_project.project.name}/web:v0.0.1", "." ]
+    }
+
+    source {
+      repo_source {
+        repo_name   = google_sourcerepo_repository.website_repo.name
+      }
+    }
+  }
+}
