9b1c8855fc23b761a3ae05e41c71dd5aa973a631
Alexandra Freeman <alexandra@hashicorp.com>
2021-03-31 10:27:13 -0400

    repo file cleanup

---
+++ build.tf
@@ -0,0 +1,27 @@
+resource "google_cloudbuild_trigger" "build" {
+  trigger_template {
+    branch_name = "master"
+    repo_name   = google_sourcerepo_repository.website_repo.name
+  }
+
+  build {
+    step {
+        name = "gcr.io/cloud-builders/npm"
+        args = ["run", "build"]
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = [ "build", "-t", "gcr.io/${data.google_project.project.name}/web:v0.0.1", "." ]
+    }
+
+    source {
+      repo_source {
+        repo_name   = google_sourcerepo_repository.website_repo.name
+      }
+    }
+
+    artifacts {
+      images = ["gcr.io/${data.google_project.project.name}/web:v0.0.1"]
+    }
+  }
+}
--- repo.tf
+++ repo.tf
@@ -6,26 +6,3 @@ output "website_repo_url" {
   value = google_sourcerepo_repository.website_repo.url
 }
 
-resource "google_cloudbuild_trigger" "build" {
-  trigger_template {
-    branch_name = "master"
-    repo_name   = google_sourcerepo_repository.website_repo.name
-  }
-
-  build {
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = [ "build", "-t", "gcr.io/${data.google_project.project.name}/web:v0.0.1", "." ]
-    }
-
-    source {
-      repo_source {
-        repo_name   = google_sourcerepo_repository.website_repo.name
-      }
-    }
-
-    artifacts {
-      images = ["gcr.io/${data.google_project.project.name}/web:v0.0.1"]
-    }
-  }
-}
